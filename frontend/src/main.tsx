fd3af93d02f3150dfb90ce4f98442bb95f027824
Nicole Molina <nicole.andrea.molina.c@gmail.com>
2026-09-11 23:00:00 +0200

    Mount planner completion controls

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import MvpControls from './MvpControls';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <MvpControls />
   </React.StrictMode>,
 );
