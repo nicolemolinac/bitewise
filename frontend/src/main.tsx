4d978da994c09bfb34bd70a8715f81116497001f
Nicole Molina <nicole.andrea.molina.c@gmail.com>
2026-09-12 01:50:41 +0200

    Remove legacy MVP controls and register PWA

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import AppV3 from './AppV3Entry';
-import MvpControls from './MvpControls';
 import ReweCatalogControl from './ReweCatalogControl';
 import './index.css';
+
+if ('serviceWorker' in navigator && import.meta.env.PROD) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').catch(() => undefined);
+  });
+}
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AppV3 />
     <ReweCatalogControl />
-    <MvpControls />
   </React.StrictMode>,
 );
