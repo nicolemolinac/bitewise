59704de03aef663365ce1baac2a451c1c097b832
Nicole Molina <nicole.andrea.molina.c@gmail.com>
2026-09-12 00:39:19 +0200

    Expose REWE catalog refresh in V3 frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import AppV3 from './AppV3Entry';
 import MvpControls from './MvpControls';
+import ReweCatalogControl from './ReweCatalogControl';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AppV3 />
+    <ReweCatalogControl />
     <MvpControls />
   </React.StrictMode>,
 );
